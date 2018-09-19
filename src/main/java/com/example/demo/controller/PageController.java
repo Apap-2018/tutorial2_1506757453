75b2c94d830fdfc6e54e7943aa49d5a697f3bfc7
JValdamer <jvaldamer@gmail.com>
2018-09-19 20:07:22 +0700

    DONE

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -32,13 +32,43 @@ public class PageController {
 		return "challenge";
 	}
 	
+	/*
+	 * generator controller
+	 */
 	@RequestMapping("/generator")
 	public String generator(@RequestParam(value = "a", required=false, defaultValue="0") String a, 
 							@RequestParam(value = "b", required=false, defaultValue="0") String b, 
 							@RequestParam(value = "result", required=false, defaultValue="hm") String result,
 							Model model) {
+		//ubah paramater a & b jadi int
+		int aInt = Integer.parseInt(a);
+		int bInt = Integer.parseInt(b);
+
+		String hm = "h";				//string untuk loop a
+		String res = "";				//string untuk loop b
+		
+		//ubah jika a atau b 0 jadi 1
+		if (a.equalsIgnoreCase("0")) {
+			aInt = 1;
+		}
+		if (b.equalsIgnoreCase("0")) {
+			bInt = 1;
+		}
+		
+		//Loop a
+		for (int i = 0; i < aInt; i++) {
+			hm = hm + "m";
+		}
+		hm = hm + " ";
+		
+		//Loop b
+		for (int i = 0; i < bInt; i++) {
+			res += hm;
+		}
+		result = res;
+			
 		model.addAttribute("a", a);
-		model.addAttribute("b", a);
+		model.addAttribute("b", b);
 		model.addAttribute("result", result);
 		
 		return "generator";
